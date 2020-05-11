bed1986c0203a78772da34871f7e0cde00ee6087
LuisDanielCL <daniel.cordero.leonhardes@gmail.com>
2020-05-11 08:01:28 -0600

    cors removed from server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors = require("cors");
 
 const port = process.env.PORT || 3000;
-app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 
